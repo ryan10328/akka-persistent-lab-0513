73ac804d2ccf9b10f9c9a956621983381abfcb9e
Ryan Tseng <ryan.tseng@hp.com>
2020-05-13 17:52:11 +0800

    Add project files.

---
+++ AkkaPersistent/FooActor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Akka.Actor;
+using Akka.Persistence;
+using Akka.Util.Internal;
+
+namespace AkkaPersistent
+{
+    public class FooActor : ReceivePersistentActor
+    {
+        private int _currentNumber = 0;
+
+        public override string PersistenceId => "foo-actor";
+
+        public FooActor()
+        {
+            Recover<int>(data =>
+            {
+                Console.WriteLine($"Recover(currentNumber): {_currentNumber}");
+                _currentNumber += data;
+            });
+
+            Command<int>(data =>
+                Persist(data, s =>
+                {
+                    Console.WriteLine($"Command(currentNumber): {_currentNumber}");
+                    _currentNumber += data;
+                })
+            );
+        }
+
+        public static Props Props()
+        {
+            return Akka.Actor.Props.Create(() => new FooActor());
+        }
+    }
+}
---
+++ AkkaPersistent/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Persistence.MongoDb;
+
+namespace AkkaPersistent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = File.ReadAllText("config.hocon");
+            var config = ConfigurationFactory.ParseString(str);
+
+            using (var actorSystem = ActorSystem.Create("root-actor", config))
+            {
+                MongoDbPersistence.Get(actorSystem);
+                var fooActor = actorSystem.ActorOf(FooActor.Props(), "foo-actor");
+
+                // fooActor.Tell(item.ToString(), ActorRefs.Nobody);
+
+                while (true)
+                {
+                    Console.WriteLine("Please press enter to send event to an actor");
+                    Console.ReadLine();
+                    fooActor.Tell(1, ActorRefs.Nobody);
+                }
+                
+                // actorSystem.WhenTerminated.Wait();
+            }
+        }
+    }
+}
